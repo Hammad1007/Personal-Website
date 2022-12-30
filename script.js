5e2cdc59eee8d48bc9bc270b71d72904bb7e8b0c
Hammad Rashid <76726810+Hammad1007@users.noreply.github.com>
2022-12-30 20:19:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 // Welcome to script.js file
 // All of the functionality is here
 // File starts here
+
+let menu = document.querySelector('#menu-icon');
+let navaber = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navaber.classList.toggle('.active');
+};
+
+const sr = ScrollReveal ({
+    distance: '80px', 
+    duration: 2800, reset: true
+})
+
+sr.reveal('.text,.social', {delay: 300, origin: 'bottom'})
+sr.reveal('.header', {delay: 100, origin: 'top'})
+sr.reveal('.text,.social', {delay: 300, origin: 'bottom'})
+
